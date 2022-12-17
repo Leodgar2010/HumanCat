80eb5b1225b8450eab3695206b47705e7c46e1f0
Leodgar2010 <leodgar2010@yandex.ru>
2022-12-17 21:05:42 +0300

    добавил абстрактный класс Animal и два интерфейса Pet и Wild

--- src/Human.java
+++ src/Human.java
@@ -37,19 +37,20 @@ public class Human {
     public void setAtHome(boolean atHome) {
         this.atHome = atHome;
     }
-    public void feedCat (Boolean atHome, Boolean hungry, Boolean sleep){
+
+    public void feedCat (String Catname,Boolean atHome, Boolean hungry, Boolean sleep){
         if (atHome && hungry && !sleep) {
-            System.out.println("Котика покормили");
+            System.out.println(Catname+ " покормлен(а)");
         }else {
-            System.out.println("Котика не покормили");}
+            System.out.println(Catname + " не покормлен(а)");}
     }
 
-    public void hugCat (Boolean atHome, Boolean catwishYoHug, Boolean humanWishtoHug, Boolean sleep)
+    public void hugCat (String Catname, Boolean atHome, Boolean catwishYoHug, Boolean humanWishtoHug, Boolean sleep)
     {
         if (atHome && catwishYoHug && humanWishtoHug&& !sleep) {
-            System.out.println("Котика обняли");
+            System.out.println(Catname + " обнят(а)");
         }else {
-            System.out.println("Котика не обняли");}
+            System.out.println(Catname  + " не обнят(а)");}
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,21 @@ public class Main {
         Human human  = new Human();
         human.setAtHome(true);
         human.setWishToHug(true);
+        System.out.println("Поговорим про котика.");
         Cat cat = new Cat();
+        cat.setName("Арсик");
         cat.setHungry(true);
         cat.setSleep(false);
         cat.setWishToHug(true);
-
-        human.feedCat(human.atHome,cat.hungry,cat.sleep);
-        human.hugCat(human.atHome,cat.wishToHug,human.wishToHug,cat.sleep);
+        human.feedCat(cat.getName(), human.atHome,cat.hungry,cat.sleep);
+        cat.feed();
+        human.hugCat(cat.getName(),human.atHome,cat.wishToHug,human.wishToHug,cat.sleep);
+        cat.play();
+        System.out.println("Теперь поговорим про волка.");
+        Wolf wolf = new Wolf();
+        wolf.setName("Вован");
+        wolf.hide();
+        wolf.bite();
 
 
     }
