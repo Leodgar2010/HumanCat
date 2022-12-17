b4f53e407135ffdd469e40ea3f8f729191df456b
Leodgar2010 <leodgar2010@yandex.ru>
2022-12-17 10:59:00 +0300

    внес правки по замечаниям преподавателя

--- src/Human.java
+++ src/Human.java
@@ -37,5 +37,19 @@ public class Human {
     public void setAtHome(boolean atHome) {
         this.atHome = atHome;
     }
+    public void feedCat (Boolean atHome, Boolean hungry, Boolean sleep){
+        if (atHome && hungry && !sleep) {
+            System.out.println("Котика покормили");
+        }else {
+            System.out.println("Котика не покормили");}
+    }
+
+    public void hugCat (Boolean atHome, Boolean catwishYoHug, Boolean humanWishtoHug, Boolean sleep)
+    {
+        if (atHome && catwishYoHug && humanWishtoHug&& !sleep) {
+            System.out.println("Котика обняли");
+        }else {
+            System.out.println("Котика не обняли");}
+    }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Human human  = new Human();
-        human.setAtHome(false);
+        human.setAtHome(true);
         human.setWishToHug(true);
         Cat cat = new Cat();
         cat.setHungry(true);
         cat.setSleep(false);
         cat.setWishToHug(true);
-        Interaction interaction = new Interaction();
-        interaction.feedCat(human.atHome,cat.hungry,cat.sleep);
-        interaction.hugCat(human.atHome,cat.wishToHug,human.wishToHug,cat.sleep);
+
+        human.feedCat(human.atHome,cat.hungry,cat.sleep);
+        human.hugCat(human.atHome,cat.wishToHug,human.wishToHug,cat.sleep);
 
 
     }
